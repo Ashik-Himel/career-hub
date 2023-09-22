e77685851290211cb7a88deedb285255ff9bf3d5
Ashik-Himel <ashikujjamanhimel@gmail.com>
2023-09-23 01:39:12 +0600

    home and jobs page created

---
+++ src/pages/Jobs.jsx
@@ -0,0 +1,11 @@
+import FeaturedJobs from "../components/FeaturedJobs";
+
+const Jobs = () => {
+  return (
+    <div>
+      <FeaturedJobs></FeaturedJobs>
+    </div>
+  );
+};
+
+export default Jobs;
